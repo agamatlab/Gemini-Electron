e9c5fa1671eec00be32755006b5973adfce4404a
agamatlab <icloudakberzade@gmail.com>
2025-02-07 20:17:47 -0500

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,      // if you need Node integration in your web code
+      contextIsolation: false
+    }
+  });
+  // Load your HTML file (ensure it's in your project directory)
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
